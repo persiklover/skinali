345d3b9ee777cf1662310c498f773bffe67a61c9
persiklover <dannniska@gmail.com>
2019-03-13 20:07:49 +0400

    Fix header and navigation for ie11

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
+var handle = ally.style.focusWithin();
+
 $(function() {
 
 $('.js-menu-btn').click(function() {
   $(this).toggleClass('active');
-  $('.nav').toggleClass('active');
+  $('.nav').toggleClass('nav--active');
 });
 
 });
